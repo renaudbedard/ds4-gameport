9461db07a1f2cc05d532f88edecba0b5bda8d758
renaudbedard <renaud.bedard@gmail.com>
2018-08-19 21:38:09 -0400

    Cleanup and working version!

--- ds4-gameport.ino
+++ ds4-gameport.ino
@@ -1,85 +1,72 @@
 #include <PS4USB.h>
 #include <SPI.h>
 
+// pins
+#define A_BUTTON A0
+#define B_BUTTON A1
+#define CS 6
+
+#define X_POT 0
+#define Y_POT 1
+
 USB Usb;
 PS4USB PS4(&Usb);
 
 SPISettings spiSettings(10000000, MSBFIRST, SPI_MODE0);
+uint8_t writeCommands[2] = 
+{ 
+  B00010001, // XXCCXXPP
+  B00010010 // where C is the command (01 for write) and P is the pot selector
+};
+uint8_t lastWrites[2] = { 127, 127 };
+char potAxis[2] = { 'X', 'Y' };
 
-uint8_t xAddress = B00000000;
-uint8_t yAddress = B00010000;
-uint8_t CS = 6;
-
-uint8_t lastXWrite = 127;
-uint8_t lastYWrite = 127;
-
-#define ABUTTON A0
-#define BBUTTON A1
-
-void potWrite(byte address, byte value)
+void potWrite(byte potIndex, byte value)
 {
   bool logValue = false;
-  if (address == xAddress && lastXWrite != value) 
+
+  if (lastWrites[potIndex] != value)
   {
-    Serial.print(F("X (address "));
-    Serial.print(address, BIN);
-    Serial.print(F(") = "));
-    Serial.println(value);
-    lastXWrite = value;
-    logValue = true;
-  }
-  if (address == yAddress && lastYWrite != value) 
-  {
-    Serial.print(F("Y (address "));
-    Serial.print(address, BIN);
-    Serial.print(F(") = "));
-    Serial.println(value);
-    lastYWrite = value;
+    Serial.print(potAxis[potIndex]);
+    Serial.print(F(" : "));
+    Serial.print(value);
+    lastWrites[potIndex] = value;
     logValue = true;
   }
 
   SPI.beginTransaction(spiSettings);
   digitalWrite(CS, LOW);
-  uint16_t fullWrite = address * 256 + value;
+  uint16_t fullWrite = writeCommands[potIndex] * 256 + value;
   uint16_t writeResult = SPI.transfer16(fullWrite);
   digitalWrite(CS, HIGH);
   SPI.endTransaction();
 
   if (logValue)
   {
-    SPI.beginTransaction(spiSettings);
-    digitalWrite(CS, LOW);
-    uint16_t readResult = SPI.transfer16((B00001100 + address) * 256);
-    digitalWrite(CS, HIGH);
-    SPI.endTransaction();
-
-    Serial.print(F("Written SPI string : "));
+    Serial.print(F(" | SPI string : "));
     Serial.print(fullWrite, BIN);
     Serial.println();
     
-    Serial.print(F("Write result : "));
-    Serial.print(writeResult, BIN);
-    Serial.println();
-  
-    Serial.print(F("Read result : "));
-    Serial.print(readResult, BIN);
-    Serial.println();
+    //Serial.print(F("Write result : "));
+    //Serial.print(writeResult, BIN);
+    //Serial.println();
   }
 }
 
 void setup() 
 {
   Serial.begin(115200);
+  
   if (Usb.Init() == -1) 
   {
     Serial.print(F("\r\nOSC did not start"));
     while (1); // Halt
   }
 
-  pinMode(ABUTTON, OUTPUT);
-  pinMode(BBUTTON, OUTPUT);
-  pinMode(CS, OUTPUT);
+  pinMode(A_BUTTON, OUTPUT);
+  pinMode(B_BUTTON, OUTPUT);
   
+  pinMode(CS, OUTPUT);
   SPI.begin();
 }
 
@@ -93,44 +80,45 @@ void loop()
     return;
   }
 
-  uint8_t stickX = PS4.getAnalogHat(LeftHatX);
-  uint8_t stickY = PS4.getAnalogHat(LeftHatY);
-
-  if (stickX > 137 || stickX < 117)
-  {
-    potWrite(xAddress, stickX);
-  }
+  if (PS4.getButtonPress(RIGHT))
+    potWrite(X_POT, 255);
   else if (PS4.getButtonPress(LEFT))
-    potWrite(xAddress, 0);
-  else if (PS4.getButtonPress(RIGHT))
-    potWrite(xAddress, 255);
+    potWrite(X_POT, 0);
   else
-    potWrite(xAddress, 127);
-
-  if (stickY > 137 || stickY < 117) 
   {
-    potWrite(yAddress, stickY);
+    uint8_t value = PS4.getAnalogHat(LeftHatX);
+    if (value < 122 || value > 132)
+      potWrite(X_POT, 255 - value);
+    else
+      potWrite(X_POT, 127);
   }
-  else if (PS4.getButtonPress(UP))
-    potWrite(yAddress, 255);
-  else if (PS4.getButtonPress(DOWN))
-    potWrite(yAddress, 0);
-  else
-    potWrite(yAddress, 127);  
 
+  if (PS4.getButtonPress(UP))
+    potWrite(Y_POT, 255);
+  else if (PS4.getButtonPress(DOWN))
+    potWrite(Y_POT, 0);
+  else
+  {
+    uint8_t value = PS4.getAnalogHat(LeftHatY);
+    if (value < 122 || value > 132)
+      potWrite(Y_POT, 255 - value);
+    else
+      potWrite(Y_POT, 127);
+  }
+ 
   if (PS4.getButtonClick(CIRCLE) || PS4.getButtonClick(SQUARE)) 
   {
-    digitalWrite(ABUTTON, HIGH);
+    digitalWrite(A_BUTTON, HIGH);
     Serial.println(F("A"));
   } 
   else 
-    digitalWrite(ABUTTON, LOW);
+    digitalWrite(A_BUTTON, LOW);
 
   if (PS4.getButtonClick(CROSS)) 
   {
-    digitalWrite(BBUTTON, HIGH);
+    digitalWrite(B_BUTTON, HIGH);
     Serial.println(F("B"));
   } 
   else 
-    digitalWrite(BBUTTON, LOW);
+    digitalWrite(B_BUTTON, LOW);
 }
